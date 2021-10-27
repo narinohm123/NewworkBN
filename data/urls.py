ae10c9b156bad3ec58416ab4da3714226892c058
Personal <narinohm123@gmail.com>
2021-10-27 15:55:47 +0700

    Bn

--- data/urls.py
+++ data/urls.py
@@ -1,9 +1,11 @@
+from os import stat
 from django.urls import path
 from django.urls.conf import include
 
 from . import views
 from .views import *
 from rest_framework import routers
+
 
 
 router = routers.DefaultRouter()
